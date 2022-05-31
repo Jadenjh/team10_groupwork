b81e40aa3a2a1a0daa2bcdcb34917eddad41b8cd
Jadenjh <woghks0108@snu.ac.kr>
2022-06-01 02:46:54 +0900

    branch A

--- team10_groupwork.py
+++ team10_groupwork.py
@@ -1,6 +1,17 @@
 import numpy as np
 import pandas as pd
 
+def Sol(n,df):
+    df.to_csv('output/result.csv',index = True)
+    f = open('output/result.csv', 'a')
+    for i in range(0,n):
+        print("기계 " + df.index[i] + ": 작업", min_per[i]+1)
+        f.write("기계 " + df.index[i] + "," + "작업 " + str(min_per[i]+1) + "\n")
+    print("최소비용 :", min)
+    f.write("최소비용," + str(min))
+    f.close()
+
+
 def main():
     while True:
         n =int(input("숫자를 입력하세요: "))
